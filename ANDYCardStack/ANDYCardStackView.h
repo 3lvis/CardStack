f8819c6956c3af0907bfe6a733732dc5b38169f3
Elvis Nunez <elvisnunez@me.com>
2014-03-05 00:25:02 +0100

    Add ANDYStackView.

---
+++ ANDYCardStack/ANDYCardStackView.h
@@ -0,0 +1,13 @@
+//
+//  ANDYCardStackView.h
+//  Demo
+//
+//  Created by Elvis Nunez on 3/5/14.
+//  Copyright (c) 2014 ANDY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANDYCardStackView : UIScrollView
+
+@end
---
+++ ANDYCardStack/ANDYCardStackView.m
@@ -0,0 +1,21 @@
+//
+//  ANDYCardStackView.m
+//  Demo
+//
+//  Created by Elvis Nunez on 3/5/14.
+//  Copyright (c) 2014 ANDY. All rights reserved.
+//
+
+#import "ANDYCardStackView.h"
+
+@implementation ANDYCardStackView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+    }
+    return self;
+}
+
+@end
