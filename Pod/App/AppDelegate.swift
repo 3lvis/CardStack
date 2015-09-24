997621ca55c9352b87c0e71711df83cbd02c5302
Elvis Nuñez <elvisnunez@me.com>
2015-09-24 07:53:47 +0200

    Fix code formatting

--- Pod/App/AppDelegate.swift
+++ Pod/App/AppDelegate.swift
@@ -6,16 +6,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-            self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
 
-            let layout = CardStackLayout()
-            layout.actualCellHeight = Double(UIScreen.mainScreen().bounds.height - 120.0)
-            layout.visibleCellHeight = 60.0
-            self.window?.rootViewController = RootController(layout: layout)
+        let layout = CardStackLayout()
+        layout.actualCellHeight = Double(UIScreen.mainScreen().bounds.height - 120.0)
+        layout.visibleCellHeight = 60.0
+        self.window?.rootViewController = RootController(layout: layout)
 
-            self.window!.makeKeyAndVisible()
-            
-            return true
+        self.window!.makeKeyAndVisible()
+
+        return true
     }
 }
 
